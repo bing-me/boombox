89ab764378db1a831907990ec97384de9475c218
BingYou Leong <byleong1990@gmail.com>
2023-05-02 15:51:42 +0800

    added coponents

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,13 @@
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
