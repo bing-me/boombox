e4bc365dd4307065c0f147478adf96aafe69b0d1
BingYou Leong <byleong1990@gmail.com>
2023-05-03 16:40:44 +0800

    added friends widget and fixed minor typo

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -19,7 +19,11 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
 
-  const isFriend = friends.find((friend) => friend._id === friendId);
+  /* const isFriend = friends.find((friend) => friend._id === friendId); */
+  const isFriend = Object.values(friends).find(
+    (friend) => friend._id === friendId
+  );
+  const isSelf = friendId === _id;
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -64,16 +68,18 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      {!isSelf && (
+        <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>
+      )}
     </FlexBetween>
   );
 };
