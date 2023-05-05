6941024922f393af9ef29c215d09b5d44c164091
BingYou Leong <byleong1990@gmail.com>
2023-05-05 17:38:15 +0800

    completed actual seed

--- server/index.js
+++ server/index.js
@@ -63,7 +63,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME UNCOMMENT TO SEED DATA */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+    User.insertMany(users);
+    Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
