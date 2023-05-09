8bfcf6a03bdd3f013d53706aba627e6d93b3667f
BingYou Leong <byleong1990@gmail.com>
2023-05-10 00:02:55 +0800

    added caddy and split dev, local, prod

--- Makefile
+++ Makefile
@@ -1,2 +1,24 @@
+build-dev:
+	cd client && $(MAKE) build-dev
+	cd server && $(MAKE) build
+
 run-dev:
+	docker-compose up
+
+###
+
+build-local:
+	cd client && $(MAKE) build-local
+	cd server && $(MAKE) build
+
+run-local:
+	docker-compose up
+
+###
+
+build-production:
+	cd client && $(MAKE) build-production
+	cd server && $(MAKE) build
+
+run-production:
 	docker-compose up
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,24 +38,6 @@ export const register = async (req, res) => {
   }
 };
 
-/* logging in */
-/* export const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email:email });
-    if (!user) return res.status(400).json({ msg: "User does not exist." });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
-
-    const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET);
-    delete user.password;
-    res.status(200).json({ token, user });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}
- */
 /* LOGGING IN */
 export const login = async (req, res) => {
   try {
